c3b19d00e84e40801b046177aeaf95abca515903
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-01-03 22:45:48 +0200

    Test breaking the code.

--- 02-variables/sample-types.ts
+++ 02-variables/sample-types.ts
@@ -3,6 +3,9 @@ let grade: number = 88.6;
 let firstName: String = "Charles";
 let lastName: String = "Moswane";
 
+found = 0;
+grade = "A";
+
 console.log(found);
 console.log("The grade is " + grade);
 console.log("Hi " + firstName + " " + lastName);

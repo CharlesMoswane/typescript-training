eeac9438062033b50352d2688e48a9bfc1852a7f
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-01-05 21:07:16 +0200

    Added radius and its getters and setters.

--- 05-inheritance/Circle.ts
+++ 05-inheritance/Circle.ts
@@ -1,5 +1,13 @@
 import { Shape } from "./shape";
 
 export class Circle extends Shape{
-    
+    private _radius: number;
+    public get radius(): number {
+        return this._radius;
+    }
+    public set radius(value: number) {
+        this._radius = value;
+    }
+
+    constructor(theX: number, theY: number, )
 }

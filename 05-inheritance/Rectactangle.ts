b2f9cb2e8c6a425a1bd049df3a30385e894c0642
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-01-08 23:02:26 +0200

    Deleted the Rectangle class.

--- 05-inheritance/Rectactangle.ts
+++
@@ -1,9 +0,0 @@
-import { Shape } from "./shape";
-
-export class Rectangle extends Shape{
-     constructor(theX: number, theY: number, private _width: number, private _length: number){
-        super(theX,theY);
-    }
-
-    
-}
